5dff1167b8a9b322f3fad338f3d8c7a9fdd3017b
Anestezie <anastasia.begvita@edu.md>
2026-05-14 20:36:03 +0300

    Commit

--- script.js
+++ script.js
@@ -1,30 +1,48 @@
+let cheltuieli = [];
+
 function adaugacheltuieli() {
     let suma = document.getElementById("sumaInput").value;
     let categorie = document.getElementById("categorieSelect").value;
     let descriere = document.getElementById("descriereInput").value;
-    
-    if (suma === "" && categorie === "Select option" && descriere === "") {
+
+    if (suma === "" || categorie === "0" || descriere === "") {
         alert("Completează toate câmpurile!");
         return;
     }
-    
+
+    cheltuieli.push({
+        id: Date.now(),
+        suma: Number(suma),
+        categorie: categorie,
+        descriere: descriere});
+    afisareCheltuieli();
 }
 
 function afisareCheltuieli() {
-    let listaCheltuieli = document.querySelector(".listcard");
+    let listaCheltuieli =
+    document.getElementById("listaCheltuieli");
     listaCheltuieli.innerHTML = "";
+    let total = 0;
 
-for (let i = 0; i < cheltuieli.length; i++) {
-            let c = cheltuieli[i];
-            total += c.suma;
+    for (let i = 0; i < cheltuieli.length; i++) {
 
-            let li = document.createElement('li');
-            li.innerHTML = `
-                    <div class="suma-badge">${c.suma} MDL</div>
-                    <div class="descriere-mica"> ${c.descriere}</div>
-                </div>
-                <button class="sterge-btn" onclick="stergeCheltuiala(${c.id})">✕</button>
-            `;
-            lista.appendChild(li);
-        }
+        let c = cheltuieli[i];
+        total += c.suma;
+        let li = document.createElement("li");
+        li.innerHTML = `
+            <div class="suma-badge">
+                ${c.suma} MDL
+            </div>
+            <div class="descriere-mica">
+                ${c.descriere}
+            </div>
+            <button class="sterge-btn">
+                ✕
+            </button>
+        `;
+
+        listaCheltuieli.appendChild(li);}
+
+    document.getElementById("totalIntrari")
+    .textContent = total;
 }
